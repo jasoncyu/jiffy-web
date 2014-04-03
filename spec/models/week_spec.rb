064ad43de1e5cc7713ec63ff9f2564cf94ffb1aa
Jason Yu <yujason2@gmail.com>
2014-04-03 15:20:03 -0700

    Functionality for entries across weeks.
    Another test.

--- spec/models/week_spec.rb
+++ spec/models/week_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Week do
-  pending "week.rb"
+  describe "day method" do
+    before do
+      @week = Week.new start_day: Date.parse("March 31, 2014") 
+      @days = (0..6).map{|i| @week.start_day + i.days}
+    end
+
+    it "should include days in the week" do
+      @days.each do |day|
+        @week.contains_day?(day).should == true
+      end
+    end
+  end
 end
